d94e8a7fb47cae925901fcf1db9466aadeff3bdb
Kate Ta <katetaa427@gmail.com>
2019-12-12 16:58:36 -0800

    End of Section 3

--- API/Startup.cs
+++ API/Startup.cs
@@ -31,6 +31,15 @@ namespace API
             {
                 opt.UseSqlite(Configuration.GetConnectionString("DefaultConnection"));
             });
+            services.AddCors(opt => 
+            {
+                opt.AddPolicy("CorsPolicy", policy => 
+                {
+                    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
+                });
+                
+            });
+            services.AddMvc(opt => opt.EnableEndpointRouting = false).SetCompatibilityVersion(CompatibilityVersion.Version_3_0);
             services.AddControllers();
 
         }
@@ -42,6 +51,13 @@ namespace API
             {
                 app.UseDeveloperExceptionPage();
             }
+            else {
+
+            }
+
+            app.UseCors("CorsPolicy");
+
+            app.UseMvc();
 
             app.UseHttpsRedirection();
 
